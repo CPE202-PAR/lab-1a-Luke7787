68cbd2c3f1c261fcd1e34d9ae3e78944f7c6328a
Luke Zhuang <36080881+Luke7787@users.noreply.github.com>
2021-01-11 09:00:57 -0800

    Update location_tests.py

--- location_tests.py
+++ location_tests.py
@@ -3,13 +3,17 @@
 import unittest
 from location import *
 
+
 class TestLocation(unittest.TestCase):
 
     def test_repr(self) -> None:
         loc = Location('SLO', 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
-    # Add more tests!
+        self.assertEqual(repr(loc), "Location('SLO', 35.3, -120.7)")
+
+    def test_repr_2(self):
+        loc = Location("Canada", 0.0, 0.0)
+        self.assertEqual(repr(loc),"Location('Canada', 0.0, 0.0)")
+
 
 if __name__ == "__main__":
-        unittest.main()
+    unittest.main()
