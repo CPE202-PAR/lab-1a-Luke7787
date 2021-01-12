269c642a9cf0c72c44163365f1b363b3f3a3eb4f
Luke Zhuang <36080881+Luke7787@users.noreply.github.com>
2021-01-11 16:38:22 -0800

    Update lab1a.py

--- lab1a.py
+++ lab1a.py
@@ -47,8 +47,9 @@ def reverse_list(int_list: Optional[List]) -> Optional[List]:
 def reverse_list_mutate(int_list: Optional[List]) -> None:
    """reverses a list of numbers, modifying the input list, returns None
    If list is None, raises ValueError"""
-       if int_list is None
-          raise ValueError
+   if int_list is None:
+      raise ValueError
+   else:
        new_lst = int_list[::-1]
        return new_lst
 
